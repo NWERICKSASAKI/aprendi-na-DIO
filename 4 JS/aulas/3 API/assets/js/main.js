8bf8ed4fa99f295ada88de91b3edfe805fae104d
NWErickSasaki <erick_sasaki@hotmail.com>
2025-08-16 14:49:29 -0300

    terminei js API e vou começar projeto

--- 4 JS/aulas/3 API/assets/js/main.js
+++ 4 JS/aulas/3 API/assets/js/main.js
@@ -2,15 +2,12 @@
 // Fectch API já está disponível no browser mais modernos
 // Google: Fetch API json
 
-const offset = 0
-const limit = 10
-const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 // retorna uma Promise (processamento assincrono -> nao tem resposta imediata)
 // Promise = promessa de um resultado, ou seja, uma hora você vai receber a resposta (se der tudo certo)
 // Abaixo: Recebe a resposta e então executa a função
 
-/*
+/* 
     fetch(url)
         .then(function(response){ // se der tudo certo
 
@@ -27,7 +24,7 @@ const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
         .finally(function(){ // assim que terminar tudo
             console.log('Requisição concluída')
         })
-*/
+ */
 
 /* ENCADEAMENTO DE THEN
     fetch(url)
@@ -55,40 +52,71 @@ const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     .then( (response) => response.json() ) // se der tudo certo
 */
 
-function covertPokemonToLi(pokemon){
-    return `
-        <li class="pokemon">
-            <span class="number">#001</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
-                </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+// window -  da acesso ao browser
+// document - da acesso ao HTML
+
+
+
+function covertPokemonTypesToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
 }
 
-fetch(url)
-    .then( (response) => response.json() ) // se der tudo certo
-    .then( (jsonBody) => jsonBody.results)
-    .then( (pokemons) => {
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0;
 
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
+const maxRecords = 11; // pra remover o botao de carregar mais (ficar só na 1° geração)
 
-            // window -  da acesso ao browser
-            // document - da acesso ao HTML
-            const pokemonList = document.getElementById('pokemonList')
-            pokemonList.innerHTML += covertPokemonToLi(pokemon)
-        }
 
-        debugger // caso o DevTools esteja ativo
-        console.log(pokemonsList)
-    } )
-    .catch( (error) => console.log(error) )// se der erro
+function loadPokemonItens(offset,limit){
 
-console.log('por mais que esteja declarada depois, este print vai aparecer primeiro')
+    function covertPokemonToLi(pokemon){
+        return `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map(( type ) => `<li class="type ${type}">${type}</li>` ).join(' ')}
+                    </ol>
+                    <img src="${pokemon.photo}" 
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        `
+    }
 
+
+    pokeApi.getPokemons(offset,limit).then((pokemons = []) => {
+        const newHTML = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map(( type ) => `<li class="type ${type}">${type}</li>` ).join(' ')}
+                    </ol>
+                    <img src="${pokemon.photo}" 
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('')
+        pokemonList.innerHTML += newHTML
+    })
+}
+
+loadPokemonItens(offset,limit)
+
+loadMoreButton.addEventListener("click", () => {
+    offset += limit;
+    const qtdRecordNextPage = offset + limit;
+    
+    if (qtdRecordNextPage >= maxRecords){
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset,newLimit);
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset,limit)
+    }
+})
