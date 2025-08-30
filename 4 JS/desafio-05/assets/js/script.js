0c8bb6a2348717b0baaa17404632bec9c165ab51
NWErickSasaki <erick_sasaki@hotmail.com>
2025-08-29 22:07:52 -0300

    continuando portifolio cv

--- 4 JS/desafio-05/assets/js/script.js
+++ 4 JS/desafio-05/assets/js/script.js
@@ -1,13 +1,17 @@
-async function init(){
-    return
+const html = {
+    photo:document.getElementById("profile-pic"),
 }
 
-init();
-
+async function init(){
+    const profile = await fetchProfileData()
+    html.photo.src = profile.photo;     
+}
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/NWERICKSASAKI/aprendi-na-DIO/refs/heads/main/4%20JS/desafio-05/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/NWERICKSASAKI/aprendi-na-DIO/refs/heads/main/4%20JS/desafio-05/data/perfil.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
 }
+
+init();
