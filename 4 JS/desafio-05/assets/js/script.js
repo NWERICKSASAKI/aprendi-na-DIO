ee0f402e3e675a151b909fba294fda9ae59fab58
NW <erick_sasaki@hotmail.com>
2025-08-31 13:09:48 -0300

    atualizando json

--- 4 JS/desafio-05/assets/js/script.js
+++ 4 JS/desafio-05/assets/js/script.js
@@ -1,17 +1,39 @@
-const html = {
-    photo:document.getElementById("profile-pic"),
+const state = {
+    sobreMim : document.getElementById("sobre-mim"),
+    foto : document.getElementById("profile-pic"),
+    idade : document.getElementById("idade"),
+    endereco : document.getElementById("endereco"),
+    estadoCivil : document.getElementById("estado-civil"),
+    disponibilidadeViagem : document.getElementById("disponibilidade-viajar"),
+    contato:{
+        email : document.getElementById("email"),
+        celular : document.getElementById("celular"),
+        github : document.getElementById("github-link"),
+        linkedin : document.getElementById("linkedin-link"),
+        site : document.getElementById("site-link"),
+    },
+    softSkills : document.getElementById("soft-skills-lista"),
+    experiencias : document.getElementById("experiencias-lista"),
+    formacoes : document.getElementById("formacoes-lista"),
+    certificacoes : document.getElementById("certificacoes-lista")
 }
 
-async function init(){
-    const profile = await fetchProfileData()
-    html.photo.src = profile.photo;     
-}
 
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/NWERICKSASAKI/aprendi-na-DIO/refs/heads/main/4%20JS/desafio-05/data/perfil.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
+}
+
+async function substituirInformacoes(profile) {
+   state.foto.src = profile.foto
+   state.sobreMim = profile.sobre-mim
+}
+
+async function init(){
+    const profile = await fetchProfileData()
+    substituirInformacoes(profile)
 }
 
 init();
